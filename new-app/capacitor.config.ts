191a096d21c14afc02c39896fdac7b654aaac9c4
tanush <tanush.shrivastava@gmail.com>
2024-11-22 17:16:31 -0600

    added mobile app

---
+++ new-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.netlify.studysmartapp',
+  appName: 'StudySmart',
+  webDir: 'build'
+};
+
+export default config;
